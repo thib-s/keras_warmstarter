ecc544bd41d8e54c6157d23807bd3b628a804dd3
thibaut boissin <thibaut.boissin@gmail.com>
2020-01-19 21:54:46 +0100

    untested strategies for Dense and Conv2D

--- warmstarter/loader.py
+++ warmstarter/loader.py
@@ -1,11 +1,19 @@
 from typing import NamedTuple
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Layer, Dense, Conv2D
+
+from warmstarter.strategies import warmstart_Dense, warmstart_Conv2D
 
 LayerMapping = NamedTuple("LayerMapping", [("from_layer", str), ("to_layer", str)])
+
+STRATEGIES = {
+    (Dense, Dense): warmstart_Dense,
+    (Conv2D, Conv2D): warmstart_Conv2D
+}
 
 
 def transfert_weights(from_model: Model, to_model: Model, layer_mapping: list[LayerMapping]):
     for mapping in layer_mapping:
         from_layer: Layer = from_model.get_layer(mapping.from_layer)
         to_layer: Layer = to_model.get_layer(mapping.to_layer)
+        STRATEGIES[(from_layer.__class__, to_layer.__class__)](from_layer, to_layer)
--- warmstarter/strategies.py
+++ warmstarter/strategies.py
@@ -1,22 +1,17 @@
 import numpy as np
 from tensorflow.keras.initializers import Initializer
-from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2D, Dense
 
 
-def spectral_edition(kernel, new_shape:tuple, kernel_initializer: Initializer=None):
-    # initialize new kernel
-    if kernel_initializer is None:
-        new_u = np.zeros((new_shape[0], new_shape[0]))
-        new_s = np.zeros(min(new_shape))
-        new_vh = np.zeros((new_shape[1], new_shape[1]))
-    else:
-        new_k = kernel_initializer(shape=new_shape)
-        new_u, new_s, new_vh = np.linalg.svd(new_k)
+def spectral_edition(kernel, new_kernel):
+    new_shape = new_kernel.shape
 
     u, s, vh = np.linalg.svd(kernel)
+    new_u, new_s, new_vh = np.linalg.svd(new_kernel)
 
     # transfer s
     s_inter_size = min(len(s), len(new_s))
+    # todo : this overwrite the largest sing values of new_s while keeping those might be interesting
     new_s[:s_inter_size] = s[:s_inter_size]
     inflated_s = np.zeros(new_shape)
     np.fill_diagonal(inflated_s, new_s)
@@ -33,24 +28,45 @@ def spectral_edition(kernel, new_shape:tuple, kernel_initializer: Initializer=No
     return np.matmul(new_u, np.matmul(inflated_s, new_vh))
 
 
+def warmstart_Dense(from_layer: Dense, to_layer: Dense, include_bias=True):
+    k1 = from_layer.kernel
+    k2 = to_layer.kernel
+    if include_bias:
+        # todo: real method should be to find x such that kernel*x = bias and reduce it's dimension with the kernel SVD
+        if from_layer.use_bias:
+            k1 = np.hstack([k1, from_layer.bias])
+        if to_layer.use_bias:
+            k2 = np.hstack([k2, from_layer.bias])
+    new_k2 = spectral_edition(k1, k2)
+    if include_bias and to_layer.use_bias:
+        to_layer.bias = new_k2[:, -1]
+        new_k2 = new_k2[:, :-1]
+    to_layer.kernel = new_k2
+
+
 def warmstart_Conv2D(from_layer: Conv2D, to_layer: Conv2D):
     in_kernel_shape = from_layer.kernel.shape
-    for channel in range(in_kernel_shape[2]):
-        for filter in range(in_kernel_shape[3]):
+    out_kernel_shape = to_layer.kernel.shape
+    filter_blocks = []
+    # 1. reshape all blocks
+    for filter in range(in_kernel_shape[3]):
+        channels_blocks = []
+        for channel in range(in_kernel_shape[2]):
             block = from_layer.kernel[:, :, channel, filter]
+            new_block = to_layer.kernel_initializer(to_layer.kernel_size, dtype=to_layer.dtype)
+            # todo: handle biases ( same a as dense, should be refactored in spectral edition )
+            new_block = spectral_edition(block, new_block)
+            channels_blocks.append(new_block)
+        if in_kernel_shape[2] > out_kernel_shape[2]:
+            # reshape channels
+            pass
+        # append reshaped channels
+        filter_blocks.append(channels_blocks)
+    if in_kernel_shape[3] > out_kernel_shape[3]:
+        # reshape filters
+        pass
+    # now we have the right amount of channels/filters put it in the new kernel
 
 
 if __name__ == '__main__':
-    kernel = np.random.sample((4, 3))
-    k2 = spectral_edition(kernel, (4, 3))  # no changes
-    k3 = spectral_edition(kernel, (4, 4))  # one extra input
-    k4 = spectral_edition(kernel, (4, 2))  # one input removed
-
-    k5 = spectral_edition(kernel, (5, 3))  # one extra output
-    k6 = spectral_edition(kernel, (2, 3))  # one output removed
-
-    k7 = spectral_edition(kernel, (5, 4))  # extra input & extra output
-    k8 = spectral_edition(kernel, (3, 2))  # fewer input & fewer output
-    k9 = spectral_edition(kernel, (5, 2))  # fewer input & extra output
-    k10 = spectral_edition(kernel, (5, 2))  # extra input & fewer output
     pass
